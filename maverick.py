1af1e55b3f83aacd57dab57a0f57818c173f549b
twizzlebizzle <ben-fisher@outlook.com>
2020-05-12 18:30:42 +0100

    Add files via upload
    Altered the original file to be more simple for my needs.  I just required the output of the 2 temperatures to link into other scripts.

--- maverick.py
+++ maverick.py
@@ -42,18 +42,14 @@ import string
 import os
 
 parser = argparse.ArgumentParser(description='Receives Wireless BBQ Thermometer Telegrams via RF-Receiver')
-parser.add_argument('--html', nargs='?', const='maverick.html', help='Writes a HTML file')
-parser.add_argument('--json', nargs='?', const='maverick.json', help='Writes a JSON file')
-parser.add_argument('--sqlite', nargs='?', const='maverick.sqlite', help='Writes to an SQLite Database')
-parser.add_argument('--thingspeak', nargs='?', const='maverick.thingspeak', help='Writes to ThingSpeak (enter Write-API key)')
 parser.add_argument('--debug', action='store_true', help='Generates additional debugging Output')
-parser.add_argument('--pin', default=22, type=int, help='Sets the Pin number')
+parser.add_argument('--pin', default=27, type=int, help='Sets the Pin number')
 parser.add_argument('--nosync', action='store_true', help='Always register new IDs')
-parser.add_argument('--fahrenheit', action='store_true', help='Sets the Output to Fahrenheit')
 parser.add_argument('--noappend', action='store_true', help='Don´t append to file')
-parser.add_argument('--verbose', action='store_true', help='Print more Information to stdout')
 
 options = parser.parse_args()
+
+#pintouse = 27
 
 if options.debug:
    print(options)
@@ -186,15 +182,11 @@ def get_data (bitlist):
       sensor1 = ''
    else:
       sensor1 -= 532
-      if options.fahrenheit:
-         sensor1 = (((sensor1*9)/5) +32)
 
    if sensor2 == 0:
       sensor2 = ''
    else:
       sensor2 -= 532
-      if options.fahrenheit:
-         sensor2 = (((sensor2*9)/5) +32)
 
    return [sensor1, sensor2]
 
@@ -444,168 +436,24 @@ def get_random_filename(filename):
     return filename + '_' + ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(12))
     
 def worker():
-   if options.verbose:
-      print('Main task running')
    # Hauptthread, wertet empfangene Pakete aus und verteilt an die anderen Queues
    global unit_list
-   if options.fahrenheit:
-      unit = 'F'
-   else:
-      unit = '°C'
-   while True:
-      item_time, item = packet_queue.get()
-      type, chksum_is = chksum(item)
-      temp1, temp2 = get_data(item)
-      state = get_state(item)
-      if updated(chksum_is, state, item_time):
-         if options.html != None:
-            html_queue.put((item_time, chksum_is, type, temp1, temp2))
-         if options.json != None:
-            json_queue.put((item_time, chksum_is, type, temp1, temp2))
-         if options.sqlite != None:
-            sqlite_queue.put((item_time, chksum_is, type, temp1, temp2))
-         if options.thingspeak != None:
-            thingspeak_queue.put((item_time, chksum_is, type, temp1, temp2))
-         if options.verbose:
-            print(time.strftime('%c:',time.localtime(item_time)), '-', chksum_is, '- Temperatur 1:', temp1, unit, 'Temperatur 2:', temp2, unit)
-         if options.debug:
-            print('raw:', item)
-            print('hex', bitlist_to_hexlist(item))
-      packet_queue.task_done()
+   unit = '°C'
+   #while True: ADD 3 spaces
+   item_time, item = packet_queue.get()
+   type, chksum_is = chksum(item)
+   temp1, temp2 = get_data(item)
+   state = get_state(item)
+   if updated(chksum_is, state, item_time):
+      #print((temp1))
+      #print(str(temp2))
+      #print(time.strftime('%c:',time.localtime(item_time)), '-', chksum_is, '- Temperature 1:', temp1, unit, 'Temperature 2:', temp2, unit)
+      if options.debug:
+         print('raw:', item)
+         print('hex', bitlist_to_hexlist(item))
+   packet_queue.task_done()
+   return(temp1,temp2)
 
-def json_writer():
-   # schreibt ein JSON-Logfile
-   if options.verbose:
-       print('Starting JSON writer Task')
-   if options.fahrenheit:
-      unit = '° F'
-   else:
-      unit = '°C'
-   while True:
-      item_time, chksum_is, type, temp1, temp2 =  json_queue.get()
-      set = {'time': item_time, 'checksum': chksum_is, 'type' : type, 'unit': unit, 'temperature_1' : temp1, 'temperature_2' : temp2}
-      if options.noappend:
-        tmp_filename = get_random_filename(options.json)
-        with open(tmp_filename, 'w') as json_file:
-            json_file.write(json.dumps(set))
-            json_file.flush()
-            os.fsync(json_file.fileno())
-            json_file.close()
-            os.rename(tmp_filename, options.json)
-      else:
-        with open(options.json, 'a') as json_file:
-            json_file.write(json.dumps(set) + ',')
-            json_file.flush()
-
-      json_queue.task_done()
-
-def html_writer():
-   # schreibt eine HTML-Datei (ohne Header und Footer)
-   if options.verbose:
-       print('Starting HTML writer Task')
-   html_file = open(options.html, 'a')
-   if options.fahrenheit:
-      unit = 'F'
-   else:
-      unit = '°C'
-   while True:
-      item_time, chksum_is, type, temp1, temp2 =  html_queue.get()
-      print(time.strftime('%c:',time.localtime(item_time)), type, '-', chksum_is, '- Temperatur 1:', temp1, unit, 'Temperatur 2:', temp2, unit, '<br>', file=html_file)
-      html_file.flush()
-      html_queue.task_done()
-
-def sqlite_writer():
-   # speichert die Empfangenen Daten in eine SQlite Datei
-   if options.verbose:
-       print('Starting SQLite writer Task')
-   sqlite_conn = sqlite3.connect(options.sqlite)
-   sqlite_c = sqlite_conn.cursor()
-   sqlite_c.execute('''CREATE TABLE IF NOT EXISTS maverick
-             (date integer, type text, id integer, temperature_1 real, temperature_2 real)''')
-   while True:
-      item_time, chksum_is, type, temp1, temp2 =  sqlite_queue.get()
-      sqlite_c.execute("INSERT INTO maverick VALUES (?, ?, ?, ?, ?)", (int(item_time), type,  chksum_is, temp1, temp2))
-      sqlite_conn.commit()
-      sqlite_queue.task_done()
-
-def thingspeak_writer():
-    global options
-    # postet die Daten auf Thingspeak
-    unit_list = {}
-    if options.verbose:
-        print('Starting ThingSpeak writer Task')
-    while True:
-        queueempty = False
-        # Bei neuen Daten sofort senden, ansonsten nach 15s (Mindestintervall, anonsten werden die gesendeten Daten ignoriert
-        try:
-            item_time, chksum_is, type, temp1, temp2 = thingspeak_queue.get(True, 15)
-        except queue.Empty:
-            queueempty = True
-        # Neue Daten verarbeiten
-        # Es werden maximal 4 Empfänger mit je 2 Kanälen unterstützt, da ThingSpeak 8 Kanäle (Felder) unterstützt
-        if not queueempty:
-            id = 4
-            if chksum_is in unit_list:
-                if options.verbose:
-                    print('Unit in List')
-                unit_list[chksum_is]['timestamp'] = int(item_time)
-                unit_list[chksum_is]['temp1'] = temp1
-                unit_list[chksum_is]['temp2'] = temp2
-                id = unit_list[chksum_is]['id']
-            else:
-                if len(unit_list) >= 4:
-                    if options.verbose:
-                        print('Maximum Number of Unit reached')
-                    maxage = int(time.time() - 20)
-                    oldestage = time.time()
-                    oldestchksum = 0
-                    for chksum, unit in unit_list.items():
-                        if unit['timestamp'] <= maxage and unit['timestamp'] < oldestage:
-                            oldestchksum = chksum
-                            oldestage = unit['timestamp']
-                            id = unit['id']
-                    if oldestchksum != 0:
-                        del unit_list[oldestchksum]
-                    else:
-                        print('All Slots in use, ignoring Device ', chksum_is)
-                        continue
-                else:
-                    if options.verbose:
-                        print('Adding Unit to List')
-                    slots = list(range(4))
-                    for chksum, unit in unit_list.items():
-                        slots.remove(unit['id'])
-                    id = slots[0]
-                unit_list[chksum_is] = {'timestamp': int(item_time), 'id': id, 'temp1': temp1, 'temp2': temp2}
-        # Keine neuen Daten empfangen, damit nichts verloren geht trotzdem in den letzten 30s empfangene Daten senden
-        else:
-            if options.verbose:
-                print('Nothing new received')
-        status = ''
-        fields = {}
-        tosend = False
-        # Welche Sender wurden in den letzten 30s empfangen
-        for chksum, unit in unit_list.items():
-            if unit['timestamp'] >= time.time() - 30:
-                tosend = True
-                fields.update({'field' + str(unit['id']*2+1): unit['temp1'], 'field' + str(unit['id']*2+2): unit['temp2']})
-            # Im Statusfeld wird ein Zuordnung der Kanäle zu den Sendern übermittelt
-            status += str(unit['id']*2+1) + '/' + str(unit['id']*2+2) + ':' + str(chksum) + ' '
-        if options.verbose:
-            print('Statusstring: ', status)
-        data = {'api_key':options.thingspeak, 'status': status}
-        data.update(fields)
-        params = urllib.parse.urlencode(data)
-        params = params.encode('utf-8')
-        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        request = urllib.request.Request('http://api.thingspeak.com/update', params, headers)
-        # Wenn Daten vorliegen, senden
-        if tosend:
-            if options.verbose:
-                print('Sending to ThingSpeak')
-            with urllib.request.urlopen(request) as response:
-                pass
-                #TODO Fehlerhandling
 
 pi = pigpio.pi() # connect to local Pi
 oldtick = pi.get_current_tick()
@@ -614,38 +462,13 @@ pi.set_noise_filter(options.pin, 4500, 400000)
 callback1 = pi.callback(options.pin, pigpio.EITHER_EDGE, pinchange)
 start = time.time()
 
-if options.html != None:
-   html_queue = queue.Queue()
-   html_writer_worker = threading.Thread(target=html_writer)
-   html_writer_worker.daemon = True
-   html_writer_worker.start()
 
-if options.json != None:
-   import json
-   json_queue = queue.Queue()
-   json_writer_worker = threading.Thread(target=json_writer)
-   json_writer_worker.daemon = True
-   json_writer_worker.start()
+#def worker2():
+ #worker1 = threading.Thread(target=worker)
+ #worker1.daemon = True
+ #worker1.start()
 
-if options.sqlite != None:
-   import sqlite3
-   sqlite_queue = queue.Queue()
-   sqlite_writer_worker = threading.Thread(target=sqlite_writer)
-   sqlite_writer_worker.daemon = True
-   sqlite_writer_worker.start()
-
-if options.thingspeak != None:
-    import urllib.request
-    import urllib.parse
-    thingspeak_queue = queue.Queue()
-    thingspeak_writer_worker = threading.Thread(target=thingspeak_writer)
-    thingspeak_writer_worker.daemon = True
-    thingspeak_writer_worker.start()
-
-worker1 = threading.Thread(target=worker)
-worker1.daemon = True
-worker1.start()
-
-while (1):
-   time.sleep(0.2)
-pi.stop()
+worker()
+#while (1):
+#   time.sleep(0.2)
+#pi.stop()
